5a8f4805aac2aeb7fe48dea59482e025176c2269
codeRookieErick <erickmoradev@gmail.com>
2023-10-24 17:26:58 -0400

    Adding test helper

--- dockerfile
+++ dockerfile
@@ -10,11 +10,11 @@ run mkdir -p $BUREAUCRAT_TABLES_DATABASE $BUREAUCRAT_BLOBS_ROOT
 run apt-get update
 run apt-get install -y python3.11 python3-venv python3-pip
 
-copy ./entrypoint.sh ./entrypoint.sh
-run chmod +x ./entrypoint.sh
-
 copy ./api ./api
 
 run pip install -r ./api/requirements.txt
+
+copy ./entrypoint.sh ./entrypoint.sh
+run chmod +x ./entrypoint.sh
 
 cmd ./entrypoint.sh
--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,29 +1,4 @@
-import requests
-import json
-
-port = 19760
-BASE_URL:str = f"http://localhost:{port}"
-
-def request(url:str, method:str = "get", data:str = ""):
-    full_url = f"{BASE_URL}{url}"
-    action = requests.get
-    if method == "post":
-        action = requests.post
-    elif method == "delete":
-        action = requests.delete
-    return action(full_url, data=data).text
-
-def get_lists():
-    return json.loads(request("/queues/"))
-
-def dequeue(name:str):
-    return request(f"/queues/{name}")
-
-def enqueue(name:str, content:str):
-    return request(f"/queues/{name}", method="post", data=content)
-
-def delete(name):
-    return request(f"/queues/{name}", method="delete")
+from tools import enqueue, dequeue, delete_queue, get_lists
 
 def test_on_enqueue_gets_created():
     queue_name = "test"
@@ -37,5 +12,5 @@ def test_on_dequeue_gets_created():
 
 def test_on_delete_gets_deleted():
     queue_name = "test"
-    delete(queue_name)
+    delete_queue(queue_name)
     assert queue_name not in get_lists()
