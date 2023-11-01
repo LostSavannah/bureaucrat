d389e42b46d427527ea075d9cf9d4db12ba5125a
Erick Fernando Mora Ramírez <emora@ri.gob.do>
2023-11-01 18:22:58 -0400

    Updating site

--- site/index.php
+++ site/index.php
@@ -14,17 +14,26 @@
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
       crossorigin="anonymous"></script>
 </head>
-<body>
-    <header class="text-light bg-primary p-4">
+<body class="bg-dark text-light">
+    <header class="bg-primary p-4">
         <h3>Bureaucrat</h3>
         <h6>Credential-less queues, blobs, sqlite3 tables and trees</h6>
     </header>
     <div class="container p-2">
        <div class="row">
         <div class="col-12">
-            <a 
-                href="https://hub.docker.com/r/coderookieerick/bureaucrat" 
-                class="btn btn-primary"> View in dockerhub </a>
+            <div class="d-flex flex-row justify-content-center align-items-center p-4 100-w">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="p-2">
+                        Bureaucrat is a credential-less service with queues, blobs, tables, trees.
+                    </div>
+                    <div class="p-2">
+                        <a 
+                            href="https://hub.docker.com/r/coderookieerick/bureaucrat" 
+                            class="btn btn-primary"> View in dockerhub </a>
+                    </div>
+                </div>
+            </div>
         </div>
        </div> 
     </div>
