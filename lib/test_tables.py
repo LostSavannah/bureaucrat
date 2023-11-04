1c8dce2ed2feddd8bcd0695f8fcf75570d6f9354
Erick Fernando Mora Ramírez <emora@ri.gob.do>
2023-11-04 12:17:35 -0400

    Adding tables tests

---
+++ lib/test_tables.py
@@ -0,0 +1,118 @@
+import random
+from .client.common.tables import TablesService
+
+baseUrl = "http://localhost:19760"
+service:TablesService = TablesService(baseUrl)
+
+def random_string(size:int = 100):
+    data = ''
+    for i in range(size):
+        data += chr(random.randint(ord('a'), ord('z')))
+    return data
+
+def test_get_databases_returns_list_of_strings():
+    assert isinstance(service.get_databases(), list), 'Databases is not a list of string'
+    
+def test_get_tables_of_any_database_returns_list_of_strings():
+    for i in range(100):
+        assert isinstance(service.get_tables(random_string()), list), 'Tables is not a list of string'
+
+def test_get_tables_of_any_database_updates_after_table_is_created():
+    database_name:str = 'main'
+    for i in range(100):
+        table_name:str = f'table_{random_string(20)}'
+        if table_name not in service.get_tables(database_name):
+            columns_count = random.randint(1, 5)
+            columns = {random_string(100):'varchar(128)' for i in range(columns_count)}
+            assert service.create_table(database_name, table_name, **columns)
+        service.drop_table(database_name, table_name)
+        assert table_name not in service.get_tables(database_name)
+
+def test_create_non_existing_table_returns_true():
+    database_name:str = 'main'
+    for i in range(100):
+        table_name:str = f'table_{random_string(10)}'
+        if table_name in service.get_tables(database_name):
+            continue
+        columns_count = random.randint(1, 5)
+        columns = {random_string(100):'varchar(128)' for i in range(columns_count)}
+        assert service.create_table(database_name, table_name, **columns)
+        assert service.drop_table(database_name, table_name)
+
+def test_create_existing_table_returns_false():
+    database_name:str = 'main'
+    for i in range(100):
+        table_name:str = f'table_{random_string(10)}'
+        if table_name in service.get_tables(database_name):
+            continue
+        columns_count = random.randint(1, 5)
+        columns = {random_string(100):'varchar(128)' for i in range(columns_count)}
+        service.create_table(database_name, table_name, **columns)
+        assert not service.create_table(database_name, table_name, **columns)
+        assert service.drop_table(database_name, table_name)
+
+def test_insert_valid_register_returns_true():
+    database_name:str = 'main'
+    for i in range(100):
+        table_name:str = f'table_{random_string(10)}'
+        if table_name in service.get_tables(database_name):
+            continue
+        is_number_table:bool = random.random() > 0.5
+        columns_count = random.randint(1, 5)
+        columns = {
+            random_string(100):('varchar(128)' if not is_number_table else 'int') 
+            for i in range(columns_count)
+        }
+        service.create_table(database_name, table_name, **columns)
+        register = {
+            i:(random_string(10) if not is_number_table else random.randint(1, 100)) 
+            for i in columns
+            }
+        assert service.insert(database_name, table_name, **register)
+        service.drop_table(database_name, table_name)
+
+def test_insert_many_register_returns_all_true():
+    database_name:str = 'main'
+    for i in range(10):
+        table_name:str = f'table_{random_string(10)}'
+        if table_name in service.get_tables(database_name):
+            continue
+        is_number_table:bool = random.random() > 0.5
+        columns_count = random.randint(1, 5)
+        columns = {
+            random_string(10):('varchar(128)' if not is_number_table else 'int') 
+            for i in range(columns_count)
+        }
+        service.create_table(database_name, table_name, **columns)
+        registers = [{
+            c:(random_string(10) if not is_number_table else random.randint(1, 100)) 
+            for c in columns
+            } for j in range(10)]
+        assert all(service.insert_many(database_name, table_name, registers))
+        service.drop_table(database_name, table_name)
+
+def test_get_rows_returns_values():
+    database_name:str = 'main'
+    for i in range(10):
+        table_name:str = f'table_{random_string(10)}'
+        if table_name in service.get_tables(database_name):
+            continue
+        is_number_table:bool = random.random() > 0.5
+        columns_count = random.randint(1, 5)
+        columns = {
+            random_string(10):('varchar(128)' if not is_number_table else 'int') 
+            for i in range(columns_count)
+        }
+        service.create_table(database_name, table_name, **columns)
+        items = random.randint(50, 100)
+        page_size = random.randint(5, 50)
+        registers = [{
+            c:(random_string(10) if not is_number_table else random.randint(1, 100)) 
+            for c in columns
+            } for j in range(items)]
+        service.insert_many(database_name, table_name, registers)
+        for page in range((items//page_size) + 1):
+            expected_size = min(page_size, items - (page*page_size))
+            result = service.get_rows(database_name, table_name, page, page_size)
+            assert len(result) == expected_size
+        service.drop_table(database_name, table_name)
