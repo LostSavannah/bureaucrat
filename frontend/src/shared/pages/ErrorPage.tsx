d231e049be57cd238d973d969602d40f2d2b4ab0
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-03-30 17:19:27 -0400

    Customizing error page

--- frontend/src/shared/pages/ErrorPage.tsx
+++ frontend/src/shared/pages/ErrorPage.tsx
@@ -1,6 +1,30 @@
 import {useLocation} from 'react-router'
 
+const baseName: string = import.meta.env.VITE_BASENAME;
+
+
+
 export default function ErrorPage(){
     const location = useLocation();
-    return <>Boberto detectado {location.pathname} : {import.meta.env.VITE_BASENAME}</>
+    const details: {name: string, data: string}[] = [
+        {name: "baseName", data: baseName},
+        {name: "location", data: location.pathname}
+    ]
+    return <>
+        <h4>I Kant take it anymore 🙁</h4>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                {details.map(d => <tr>
+                    <td>{d.name}</td>
+                    <td>{d.data}</td>
+                </tr>)}
+            </tbody>
+        </table>
+    </>
 }
