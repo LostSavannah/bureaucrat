672489aedb0f4a3a2505e8809d13af10074c0ee6
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-03-30 17:09:45 -0400

    Meh

--- frontend/src/shared/pages/ErrorPage.tsx
+++ frontend/src/shared/pages/ErrorPage.tsx
@@ -2,5 +2,5 @@ import {useLocation} from 'react-router'
 
 export default function ErrorPage(){
     const location = useLocation();
-    return <>Boberto detectado {location.pathname}</>
+    return <>Boberto detectado {location.pathname} : {import.meta.env.VITE_BASENAME}</>
 }
