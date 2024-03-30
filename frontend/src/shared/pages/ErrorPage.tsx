cd45d39e2e955fcaf778f39cf1250cf7c48b69ee
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-03-30 19:18:26 -0400

    Adding basename

--- frontend/src/shared/pages/ErrorPage.tsx
+++ frontend/src/shared/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import {useLocation, useRouteError} from 'react-router'
 
-const baseName: string = import.meta.env.VITE_BASENAME;
+const baseName: string = import.meta.env.VITE_BASENAME ?? "/apps/bureaucrat";
 
 
 
