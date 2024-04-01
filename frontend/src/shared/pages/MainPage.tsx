e72639666874869313a1cc1dcfeb9a687b3d4474
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-03-31 22:48:23 -0400

    Adding header

--- frontend/src/shared/pages/MainPage.tsx
+++ frontend/src/shared/pages/MainPage.tsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 
 export default function MainPage(){
     return <>
+    <div className="w-100 h4 bg-dark p-3 text-light">
+        <img className="p-1 bg-light rounded-circle" width="50" src="/bureaucrat.svg"/>
+        <span className="p-3">Bureaucrat</span>
+    </div>
     <div className="container">
         <div className="row">  
-            <div className="col-12 col-md-3">
-                <header>Bureaucrat</header>
+            <div className="col-12 col-md-2">
                 <nav>
                     <ul>
                         <li><Link to="/queues-page">Queues</Link></li>
@@ -17,7 +20,7 @@ export default function MainPage(){
                     </ul>
                 </nav>
             </div>
-            <div className="col-12 col-md-9">
+            <div className="col-12 col-md-10">
                 <Outlet></Outlet>
             </div> 
         </div>
