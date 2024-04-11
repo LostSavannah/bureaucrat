f856201e8aa23885bb48cb6eb3930c6e0d02cdd5
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-04-11 01:14:51 -0400

    Changes on Nothing.tsx

--- frontend/src/shared/components/common/Nothing.tsx
+++ frontend/src/shared/components/common/Nothing.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { choose } from "../../tools";
 
 export default function Nothing() {
-   const phrasesOfEmptiness = [
-    "A void of emotion",
-    "A vast expanse of solitude",
-    "An echoing abyss of nothingness",
-    "Loneliness consumes the soul",
-    "The silence of a desolate heart",
-    "A hollow ache within",
-    "A vacant, barren landscape",
-    "The empty echoes of a vacant room",
-    "The desolation of a deserted wasteland",
-    "A void of meaning and purpose",
-    "The hollowness of unfulfilled dreams",
-    "An empty vessel, devoid of hope",
-    "A cold and empty heart",
-    "The stark emptiness of a blank canvas",
-    "The hushed stillness of a forgotten place",
-    "A world devoid of color and life",
-    "A bottomless pit of despair",
-    "The absence of light in a dark soul",
-    "A void that swallows all joy",
-    "The emptiness of a shattered promise"
-  ];
+   const phrasesOfEmptiness = useMemo(() => [
+    "🌌 A void of silence 🌌",
+    "🌑 Emptiness echoed in the silence 🌑",
+    "🕳️ The hollow emptiness of solitude 🕳️",
+    "🌫️ A vast expanse of nothingness 🌫️",
+    "🐚 An empty shell of a soul 🐚",
+    "🌀 The abyss of emptiness 🌀",
+    "🌾 Echoes of loneliness 🌾",
+    "🌀 A vacuum of meaning 🌀",
+    "💔 Desolate wasteland of the heart 💔",
+    "🔇 A hollow echo in the void 🔇",
+    "❌ The absence of purpose ❌",
+    "👁️ A vacant stare into oblivion 👁️",
+    "🚢 An empty vessel 🚢",
+    "🌌 Loneliness echoed in the emptiness 🌌",
+    "💭 The void of lost dreams 💭",
+    "📢 The echo of emptiness reverberates 📢",
+    "😔 The vacuum of despair 😔",
+    "🏜️ The barren landscape of the soul 🏜️",
+    "🗣️ The echo of nothingness 🗣️",
+    "🌌 The desolation of empty spaces 🌌"
+  ], []);
   
   const [phrase, setPhrase] = useState("");
 
   useEffect(() => {
     setPhrase(choose(phrasesOfEmptiness));
-  }, []);
+  }, [phrasesOfEmptiness]);
 
   return (
-    <div className="d-flex flex-column align-items-center p-4 alert alert-primary">
+    <div className="d-flex flex-column align-items-center p-4 alert alert-dark">
         "{phrase}"
     </div>
   )
