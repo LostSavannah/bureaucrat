e254acd260d17f7b9655c340eeea83b44c56df56
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-04-15 20:09:21 -0400

    Update scripts

---
+++ scripts/linux/run.sh
@@ -0,0 +1,1 @@
+docker run -itd --name bureaucrat -p 19970:19970 bureaucrat
--- scripts/reset.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call build.bat
-echo stopping previowsly running container...
-docker stop bureaucrat > nul
-echo removing container...
-docker rm bureaucrat > nul
-run.bat > nul
